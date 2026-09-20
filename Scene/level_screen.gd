26ac7d098858fd692d521d06d9b599a5e951729a
bluecore <bluecorexux@gmail.com>
2026-09-20 16:38:09 +0530

    refactor: rename level_scene/timer_screen to level_screen and fix scene paths

--- Scene/level_screen.gd
+++ Scene/level_screen.gd
@@ -12,10 +12,9 @@ var time
 
 func _ready() -> void:
 	await Timer(5.0) # using the function created
-	
 	if Global.minigames_done < 3: # if you havent completed 3 minigames yet 
 		Global.minigames_done = Global.minigames_done +1
-		get_tree().change_scene_to_file("res://scenes/minigame_" + str(Global.minigames_done) + ".tscn") # changes your scene by arranging this frankenstein path. 
+		get_tree().change_scene_to_file("res://Scene/minigame_" + str(Global.minigames_done) + ".tscn") # changes your scene by arranging this frankenstein path. 
 # Above, your script is being told to go to the next minigame. If the 
 # current minigame is Level 1, then you would be on minigame 1. If you 
 # complete that level, you have the minigames_done add one, and then you 
@@ -23,10 +22,10 @@ func _ready() -> void:
 # should be next. Make sure you name your minigame saves appropriately.
 
 	else:
-		get_tree().change_scene_to_file("res://scenes/title_screen.tscn") # changes your scene
+		get_tree().change_scene_to_file("res://Scene/title_screen.tscn") # changes your scene
 	
 
-func _process(delta: float) -> void: # runs EVERY FRAME
+func _process(_delta: float) -> void: # runs EVERY FRAME
 	match Global.lives: # asks or checks if lives is equal to one of 
 #these values, cool hack. by the way this is a horrid way to illustrate the 
 #lives visually so later you can always find alternative code. Now, dw abt it.
