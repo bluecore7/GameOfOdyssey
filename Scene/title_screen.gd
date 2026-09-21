9cc461ca3717f4057bbeb8b3274b018c5934be3e
bluecore <bluecorexux@gmail.com>
2026-09-21 18:41:12 +0530

    feat: add typewriter SFX and button blip to title screen

--- Scene/title_screen.gd
+++ Scene/title_screen.gd
@@ -1,17 +1,34 @@
 extends Node2D
 
+@onready var subtitle: RichTextLabel = $Subtitle
+@onready var type_key: AudioStreamPlayer = $TypeKey
+@onready var button_blip: AudioStreamPlayer = $ButtonBlip
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
 
+func _ready() -> void:
+	type_out(subtitle, 0.08)
+
+func type_out(label: RichTextLabel, delay: float) -> void:
+	label.visible_characters = 0
+	var total = label.get_total_character_count()
+	for i in range(total + 1):
+		label.visible_characters = i
+		if i > 0:
+			type_key.play()
+		await get_tree().create_timer(delay).timeout
 
 func _on_start_pressed() -> void:
-	print("Hi i am pressed")
-	get_tree().change_scene_to_file("res://Scene/level_screen.tscn")
+	button_blip.play()
 	
+	get_tree().change_scene_to_file("res://Scene/level_screen.tscn")
+
+func _on_settings_pressed() -> void:
+	button_blip.play()
+	print("Settings pressed")
+
+func _on_quit_pressed() -> void:
+	button_blip.play()
+	await get_tree().create_timer(0.1).timeout
+	get_tree().quit()
